29fb9be9bad28646900d781676fdf5d2c2d2cace
Satiroo75 <mirkocastrianni@gmail.com>
2026-06-25 10:28:18 +0200

    Add files via upload

--- app/regole/page.tsx
+++ app/regole/page.tsx
@@ -1,14 +1,4 @@
 import Link from 'next/link';
-
 export default function Page() {
-  return (
-    <main className="page">
-      <section className="panel">
-        <h1>Regole del sito</h1>
-        <p>Questo è uno spazio libero, dove ognuno può esprimere le sue opinioni, di qualsiasi genere siano. Vige una sola regola, lo si deve fare in un contesto civile. Rispetta te stesso e gli altri e farai parte di questa comunità che da moltissimi anni segue le vicende del Catania, e si riunisce regolarmente per scambiare quattro chiacchiere tra amici.</p>
-        
-        <Link className="back" href="/">Torna alla Home</Link>
-      </section>
-    </main>
-  );
+  return <main className="page"><section className="panel"><h1>Regole del sito</h1><p>Questo è uno spazio libero, dove ognuno può esprimere le sue opinioni, di qualsiasi genere siano. Vige una sola regola, lo si deve fare in un contesto civile. Rispetta te stesso e gli altri e farai parte di questa comunità che da moltissimi anni segue le vicende del Catania, e si riunisce regolarmente per scambiare quattro chiacchiere tra amici.</p><Link className="back" href="/">Torna alla Home</Link></section></main>;
 }

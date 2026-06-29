47ae56a08e8a04e6ee22597824f06d714c99bf49
Satiroo75 <mirkocastrianni@gmail.com>
2026-06-29 18:11:46 +0200

    Add files via upload

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,14 @@
 import Link from 'next/link';
-export default function Page(){
-  return <main className="page"><section className="panel"><h1>Login</h1><p>Login in preparazione.</p><Link className="back" href="/">Torna alla Home</Link></section></main>
+
+export default function Page() {
+  return (
+    <main className="page">
+      <section className="panel">
+        <h1>Login</h1>
+        <p>Login in preparazione.</p>
+        
+        <Link className="back" href="/">Torna alla Home</Link>
+      </section>
+    </main>
+  );
 }
